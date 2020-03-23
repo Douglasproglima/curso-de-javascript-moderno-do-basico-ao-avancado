9cbbc59df12bef7936151a8baf022cd67723bcbf
Douglas Lima <douglasproglima@gmail.com>
2020-03-23 20:43:18 -0300

    The main difference between VAR and LET

---
+++ 09_aula/js/main.js
@@ -0,0 +1,9 @@
+//VAR Permite redeclarar o valor da variável
+var name = 'Var Douglas';
+var name = 'Var NovoNome';
+console.log(name);
+
+//Não é possível redeclarar uma variável com o mesmo nome.
+let lastName = 'Let Douglas';
+let lastName = 'Let NovoNome';
+console.log(lastName);
