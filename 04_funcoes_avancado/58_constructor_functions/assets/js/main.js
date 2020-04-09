780905d9807c2c392ff2f5fefce5a2f42f675f88
Douglas Lima <douglasproglima@gmail.com>
2020-04-09 11:30:27 -0300

    Funções Construtoras - Objetos da OO

---
+++ 04_funcoes_avancado/58_constructor_functions/assets/js/main.js
@@ -0,0 +1,25 @@
+/*
+CONSTRUCTOR FUNCTIONS - Funções construtoras criam e retornam os objetos
+OBSERVACAO: Na função construtora SEMPRE deve iniciar a primera letra com letra maiuscula
+EXEMPLO:
+  Factory Function....: createPeople(...){....};
+  Constructor Function: const nameVariavel = new People(...);
+*/
+function People(name, firstName){
+  //Atributos/Métodos Privados
+  const ID = 1;
+  const metodoPrivado = function() { console.log(`${this.name } : Sou método privado.`) };
+  
+  //Atributos/Métodos Públicos
+  this.name = name;
+  this.firstName = firstName;
+
+  this.metodoPublico = function(){ console.log(`${ID} ${this.name } ${this.firstName}: Sou método público.`); };
+}
+
+const people1 = new People('Douglas', 'Lima');
+//console.log(people1.metodoPrivado);
+
+const people2 = new People('Ótavio', 'Berk');
+console.log(people2.metodoPublico());
+/* */
