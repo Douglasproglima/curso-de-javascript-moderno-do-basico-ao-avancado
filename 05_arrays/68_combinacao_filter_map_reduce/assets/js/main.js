ad8182bb4c050abd673f0e432e517719f810a369
Douglas Lima <douglasproglima@gmail.com>
2020-04-12 21:10:04 -0300

    Refatoração - Filter().Map().Reduce()

--- 05_arrays/68_combinacao_filter_map_reduce/assets/js/main.js
+++ 05_arrays/68_combinacao_filter_map_reduce/assets/js/main.js
@@ -10,30 +10,32 @@ console.log(' ');
         -> Dobrar o valor dos numeros
         -> Reduzir ou somar todos os numeros
 */
-const numbersPar = arrayNumber.filter(el => {
-    if(el % 2 === 0) return el
-});
+const numbersPar = arrayNumber.filter(el => el % 2 === 0);
 console.log('----------------------------Números Pares----------------------------');
 console.log(numbersPar);
 console.log(' ');
 
-const doubleNumbersPar = numbersPar.map(el => el * 2 );
-console.log('----------------------------DOBRO----------------------------');
+const doubleNumbersPar = numbersPar.map(el => el * 2);
+console.log('----------------------------Dobro----------------------------');
 console.log(doubleNumbersPar);
 console.log(' ');
 
-const totalNumbersPar = doubleNumbersPar.reduce((acumulate, obj) => acumulate + obj ); 
-console.log('----------------------------SOMA----------------------------');
+const totalNumbersPar = doubleNumbersPar.reduce((acumulator, el) => acumulator + el);
+console.log('----------------------------Soma----------------------------');
 console.log(totalNumbersPar);
 console.log(' ');
 
 
 const arrayNumber1 = [6, 7, 8, 9, 1026, 1, 41, 56, 2 ,3 ,4, 129, 5, 10, 1483.45, 21, 29, 35];
-const allOptions = arrayNumber1
-    .filter( el => el % 2 === 0)
-    .map(el => el * 2)
-    .reduce((acumulate, el) => acumulate + el);
+console.log('----------------------------ORIGINAL----------------------------');
+console.log(arrayNumber1);
+console.log(' ');
 
-console.log('----------------------------SOMA----------------------------');
+//Tudo em apenas uma expressão
+const allOptions = arrayNumber1
+    .filter(el => el % 2 === 0)
+    .map(el => el * 2)
+    .reduce((acumulator, el) => acumulator + el)
+console.log('----------------------------TODAS EXPRESSÕES----------------------------');
 console.log(allOptions);
 console.log(' ');
