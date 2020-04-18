1a48e7d377658c47915b02d96073fdcacf0291fe
Douglas Lima <douglasproglima@gmail.com>
2020-04-17 22:14:10 -0300

    Validação de inputs por Classes e Métdos

---
+++ 07_classes/85_validar_formcpf_usando_classe/assets/js/validate_cpf.js
@@ -0,0 +1,50 @@
+class ValidateCPF {
+    constructor(cpf) {
+        Object.defineProperty(this, 'cleanCPF', {
+            writable: false, //Não pode escrever
+            enumerable: true, //True: irá aparecer no console.log
+            configurable: false,
+            value: cpf.replace(/\D+/g, '')
+        });
+    }
+
+    partialCPF() {
+        return this.cleanCPF.slice(0, -2);
+    }
+
+    isSequenceValid() {
+        const sequence = this.cleanCPF.charAt(0).repeat(11);
+        return this.cleanCPF === sequence; 
+    }
+
+    initialValidade() {
+        if(!this.cleanCPF) return false;
+        if(this.cleanCPF.length !== 11) return false;
+        if(this.cleanCPF === 'undefined') return false;
+        if(typeof this.cleanCPF !== 'string') return false;
+    }
+
+    validate() {
+        this.initialValidade();
+        if(this.isSequenceValid()) return false;
+        const partialCPF = this.partialCPF();
+        const firstDigit = ValidateCPF.getDigit(partialCPF);
+        const secondDigit = ValidateCPF.getDigit(partialCPF + firstDigit);
+        const newCPF = partialCPF + firstDigit + secondDigit;
+
+        return newCPF === this.cleanCPF;
+    }
+
+    //Método Static pois não precisa de nada da instancia, tudo é passado como parametro
+    static getDigit(partialCPF){
+        let sumDigit = 0;
+        let countdown = partialCPF.length + 1;
+        for(let strNumeric of partialCPF){
+            sumDigit += (countdown * parseInt(strNumeric))
+            countdown--;
+        }
+
+        const digit = 11 - (sumDigit % 11);
+        return digit > 9 ? '0' : String(digit);
+    }
+}
