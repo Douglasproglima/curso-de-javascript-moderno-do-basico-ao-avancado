929795bc0ccf64d756d4384466950f6751329da9
Douglas Lima <douglasproglima@gmail.com>
2020-05-01 17:15:55 -0300

    Basic Template Express with Webpack

---
+++ 10_nodeJS/12_express_webpack/frontend/modules/promises.js
@@ -0,0 +1,13 @@
+function promiseExemples() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Hello World');
+            resolve();
+        });
+    }, 2000);
+}
+
+export default async function() {
+    await promiseExemples();
+    console.log('Finish');
+}
