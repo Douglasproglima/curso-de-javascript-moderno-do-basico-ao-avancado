a353c8de43333a8c8933beb86dbce3c219329c3f
Douglas Lima <douglasproglima@gmail.com>
2020-05-16 15:49:28 -0300

    Refatoração ContactModel

--- 11_projeto_agenda/src/models/ContactModel.js
+++ 11_projeto_agenda/src/models/ContactModel.js
@@ -70,20 +70,4 @@ Contact.delete = async function(id) {
     return contact;
 };
 
-Contact.formatPhoneNumber = function(str) {
-    let cleaned = ('' + str).replace(/\D/g, '');
-  
-  //Check if the input is of correct
-  let match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
-  
-  if (match) {
-    //Remove the matched extension code
-    //Change this to format for any country code.
-    let intlCode = (match[1] ? '+1 ' : '')
-    return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('')
-  }
-  
-  return null;
-}
-
 module.exports = Contact;
