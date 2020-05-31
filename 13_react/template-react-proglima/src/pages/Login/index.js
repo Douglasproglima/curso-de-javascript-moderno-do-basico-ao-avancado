62bcd7ad0763bb4d18b07cdd01849bd652a595e5
Douglas Lima <douglasproglima@gmail.com>
2020-05-31 20:37:15 -0300

    Configurando Axios x API Virutal School Local

--- 13_react/template-react-proglima/src/pages/Login/index.js
+++ 13_react/template-react-proglima/src/pages/Login/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
+import axios from '../../services/axios';
 
 export default function Login() {
+  React.useEffect(() => {
+    async function getDate() {
+      const response = await axios.get('/students/');
+      const { data } = response;
+      console.log(data);
+    }
+
+    getDate();
+  }, []);
+
   return (
     <Container>
       <Title isRed={false}>
